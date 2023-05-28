0f41e7dc39bc1ae8e62b2a25fe9129032f61d79c
ArjunBhandari2511 <110237796+ArjunBhandari2511@users.noreply.github.com>
2023-05-28 14:50:17 +0530

    Create SelectionSort.py

---
+++ SelectionSort.py
@@ -0,0 +1,13 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+arr = [5,4,3,2,1]
+sorted_arr = selection_sort(arr)
+print(sorted_arr)
